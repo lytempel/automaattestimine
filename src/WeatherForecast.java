3739e0a603655a2ebd2a12c79c20b3041ca1430a
lytempel <lytempel@gmail.com>
2017-09-25 00:48:51 +0300

    Initial commit

---
+++ src/WeatherForecast.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Ly Tempel on 24.09.2017.
+ */
+public class WeatherForecast {
+}
---
+++ tests/WeatherForecastTest.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ly Tempel on 24.09.2017.
+ */
+public class WeatherForecastTest {
+    @Test
+    public void testIfThereIsInternetConnection() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfWeatherForecastAPIIsAvailable() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfCurrentWeatherInformationIsAvailable() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfCurrentTemperatureIsAvailable() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfThreeDayForecastIsAvailable() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfMaxTemperatureIsAvailable() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfMinTemperatureIsAvailable() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfGEOIsAvailable() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testIfGEOIsInCorrectFormat() {
+        fail("Not implemented yet");
+    }
+
+}
